0f5c791b295b60c22931613845773e7b9f04c0fb
Rex Lee(李俊) <duguying2008@gmail.com>
2016-10-10 13:08:24 +0800

    add readme and version

--- calendar.go
+++ calendar.go
@@ -11,6 +11,9 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Calendar"
 	app.Usage = "My Calendar!"
+	app.Version = "1.0.1010"
+	app.Author = "独孤影"
+	app.Email = "root@duguying.net"
 
 	app.Commands = []cli.Command{
 		cmd.CmdServe,
